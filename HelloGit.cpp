f6767ec23b367508026acba9ecff8a2df8de1701
Colton Kemp <61294073+Colton-MK@users.noreply.github.com>
2025-04-24 02:28:19 -0400

    Hello Git has Name Asking and Greeting Function

--- HelloGit.cpp
+++ HelloGit.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <string>
 
 int main(){
-    std::cout << "Hello Git World" << std::endl;
+    introduction();
+    return 0;
+}
 
+void introduction(){
+    string username;
+    std::cout << "Hello Git World" << std::endl;
+    std::cout << "What's your name?" << std::endl;
+    std::cout << "Please enter your name: " << std::endl;
+    std::cin >> username;
+    std::cout << "Hi " << username << "! It's VERY nice to me you :)" << std::endl;
     return 0;
 }
